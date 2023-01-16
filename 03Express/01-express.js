d6652735b12f6cb878d2915b9a893d24601ebc5c
JaviAres <jam061097@gmail.com>
2023-01-16 20:21:09 +0100

    Finsihing pokedex

--- 03Express/01-express.js
+++ 03Express/01-express.js
@@ -1,6 +1,32 @@
 const express = require('express')
+const bodyParser=require('body-parser')
 const app = express()
-const port = 3005
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json())
+require('dotenv').config()
+const port=process.env.PORT||3005
+/*Conexión a la Base de Datos locales
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test',{userNewUrlParser: true, useUnifiedTopology: true});
+*/
+
+//Conexión a base de datos remotas
+const mongoose = require('mongoose');
+//Variables que tendremos siempre:
+//Lo correcto será declararlas EN VARIABLES DE ENTORNO
+//para que nadie vea directamente nuestras credenciales
+
+const uri =`mongodb+srv://${process.env.db_user}:${process.env.db_password}@cluster0.ut6txeq.mongodb.net/${process.env.db_name}?retryWrites=true&w=majority`; //URL de conexión, que completaremos luego
+
+mongoose.connect(uri,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+  .then(() => console.log('Base de datos conectada'))
+  .catch(e => console.log(e))
+
+
+
 app.set('views', __dirname+'/views')
 app.set('view engine', 'ejs')
 
